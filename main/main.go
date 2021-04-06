352a47b73db9f6d8f7755643e6a73b3b6da1837f
lbh2001 <1178055813@qq.com>
2021-04-06 23:27:45 +0800

    update test

--- main/main.go
+++ main/main.go
@@ -5,16 +5,36 @@ import (
 )
 
 func main() {
-	// 创建一个默认的路由引擎
+
 	r := gin.Default()
-	// GET：请求方式；/hello：请求的路径
-	// 当客户端以GET方法请求/hello路径时，会执行后面的匿名函数
+
 	r.GET("/hello", func(c *gin.Context) {
 		// c.JSON：返回JSON格式的数据
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
+			"data":    "gin go go ",
 		})
 	})
-	// 启动HTTP服务，默认在0.0.0.0:8080启动服务
+
+	r.POST("/book", func(c *gin.Context) {
+
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		c.GetRawData()
+
+		c.JSON(200, gin.H{
+			"message":  "this is POST method",
+			"username": username,
+			"password": password,
+		})
+	})
+
+	r.GET("/urlQuery", func(c *gin.Context) {
+		name := c.DefaultQuery("name", "default name")
+		c.JSON(200, gin.H{
+			"name": name,
+		})
+	})
+
 	r.Run()
 }
